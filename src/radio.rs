111a8092906f0b6ec7df0cba9ae290724883336a
Simon Molenkamp <s.h.molenkamp@student.tudelft.nl>
2025-03-17 13:09:56 +0100

    leds to check

--- src/radio.rs
+++ src/radio.rs
@@ -1,6 +1,7 @@
 use nrf51_pac::RADIO;
 use crate::flash::FlashError;
 use crate::led::Led;
+use crate::time::delay_ms_assembly;
 
 pub enum RadioError {
 
@@ -13,8 +14,12 @@ pub(crate) fn initialize (radio: RADIO) -> Result<(), FlashError> {
     let on = radio.power.read().power().is_enabled();
     if on == true {
         Led::Green.on();
+        delay_ms_assembly(10);
+        Led::Green.off();
     } else {
         Led::Red.on();
+        delay_ms_assembly(10);
+        Led::Red.off();
     }
     Ok(())
 }
